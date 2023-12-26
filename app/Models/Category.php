790b34561f4b1e784c6bd7a318c3cc4cf7d8c310
NgQuangHuy2010 <nqht123456789@gmail.com>
2023-12-26 16:31:04 +0700

    abc

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $table = "category";
-    protected $fillable = ["name","image", "keyword", "desc", "level","status"];
+    protected $fillable = ["name", "image", "keyword", "desc", "level", "status"];
     protected $primarykey = "id";
     public $timestamps = false;
 }
