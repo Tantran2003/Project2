d95e9bd22b65c48ad22c1bab21d0f438a702363f
tan110103 <trannhattan805@gmail.com>
2024-01-23 23:59:57 +0700

    Update: Finish Contact

--- app/Http/Controllers/Interface/ContactController.php
+++ app/Http/Controllers/Interface/ContactController.php
@@ -5,31 +5,29 @@ namespace App\Http\Controllers\Interface;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Contact;
-use Illuminate\Support\Carbon;
 class ContactController extends Controller
 {
     public function index()
     {
-        return view('interface/contact/contact');
+        return view('interface/pages/contact');
     }
 
-    public function contact(Request $request)
+    public function store(Request $request)
     {
-        $validate = $request->validate([
-            'name' => 'required',
-            'email' => 'required|email',
-            'phone' => 'required|numeric|digits:10',
-            'comment' => 'required'
-        ]);
-        $current = new Carbon();
+        // $validate = $request->validate([
+        //     'name' => 'required',
+        //     'email' => 'required|email',
+        //     'phone' => 'required|numeric|digits:10',
+        //     'comment' => 'required'
+        // ]);
         $contact = new Contact();
         $contact->name = $request->name;
         $contact->email = $request->email;
         $contact->phone = $request->phone;
         $contact->comment = $request->comment;
-        $contact->date = $current;
-
+     
         $contact->save();
-        return redirect()->route('interface/contact/contact')->with('msg','Contact successfully !!! ');
+
+        return redirect()->route('gd.contactindex')->with('success', "Gửi thành công");
     }
 }
